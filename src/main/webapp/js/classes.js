a0e2e3dccd1ff5617e841b1501d0b0f2a982b14e
mitjanikolaus <mitjanikolaus@gmail.com>
2014-11-07 12:39:28 +0100

    added space between dummy type and id

--- src/main/webapp/js/classes.js
+++ src/main/webapp/js/classes.js
@@ -385,7 +385,7 @@ function(Utils, Server){
 		
 		//TODO: make dynamic
 		if(node.name === "TU Berlin"){
-			var type = $("<td>").html("robot(0135-2135-2436-2416)");
+			var type = $("<td>").html("robot (0135-2135-2436-2416)");
 			
 			var configureButton = $("<button>").addClass("btn").attr("data-toggle","collapse").attr("data-target","#configureResource412").append("Configure", $("<span>").addClass("caret"));
 			
@@ -400,7 +400,7 @@ function(Utils, Server){
 			provisionTable.append(tableRow);
 		}
 		if(node.name === "UCT"){
-			var type = $("<td>").html("OpenMTC-as-a-Service(5335-3547-2352-5356)");
+			var type = $("<td>").html("OpenMTC-as-a-Service (5335-3547-2352-5356)");
 			
 			var configureButton = $("<button>").addClass("btn").attr("data-toggle","collapse").attr("data-target","#configureResource323").append("Configure", $("<span>").addClass("caret"));
 			
