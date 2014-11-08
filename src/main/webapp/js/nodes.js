97a9ac4c225771b3092fbe2a7df9bfdda8332b88
mitjanikolaus <mitjanikolaus@gmail.com>
2014-11-08 14:17:43 +0100

    cleanup of nodes.js

--- src/main/webapp/js/nodes.js
+++ src/main/webapp/js/nodes.js
@@ -8,49 +8,48 @@ function(Utils, Server){
 		case "FEDERATION_ADMIN":
 			createAllNodesPage();
 			createAddNodePage();
-			initAllNodesAside();
-			initAllNodes(false);
-			createAddNodePage();
+			createAllNodesAside();
 			break;
 		case "NODE_ADMIN":
 			createManageNodePage(Utils.getCurrentUser().node.name);
-			initNodeAside(Utils.getCurrentUser().node.name);
+			createNodeAside(Utils.getCurrentUser().node.name);
 			break;
 		}
 	};
 	
-	initAllNodesAside = function(){
+	createManageNodePage = function(nodename){
+		var header = $("<h3>").html(nodename);
+		var subheader = $("<h4>").html("Manage your node here");
+		var manage_node_page = $("<div>").attr("id", "manage_node").addClass("row-fluid tab-pane").append(header, subheader, $("<hr>"));
+		
+		$("#desktop").append(manage_node_page);
+	}
+	
+	createNodeAside = function(nodeName){
+		var nodesHeader = "<h4><i class='fa fa-sitemap fa-lg'></i>Nodes</h4>";
+		var node = $("<div>").append($("<ul>").addClass("fa-ul navigationLink").append("<li><a href='#manage_node'><i class='fa fa-minus fa-li'></i>"+nodeName+"</a></li>"));
+		$("#homeAside").append($("<div>").append(nodesHeader,node));
+	};
+	
+	createAllNodesAside = function(){
 		var nodesHeader = "<h4><i class='fa fa-sitemap fa-lg'></i>Nodes</h4>";
 		var nodes = $("<div>").append($("<ul>").addClass("fa-ul navigationLink").append("<li><a href='#nodes'><i class='fa fa-minus fa-li'></i>All nodes</a></li>","<li><a href='#addnode'><i class='fa fa-plus fa-li'></i>Add node</a></li>"));
 
 		$("#homeAside").append($("<div>").append(nodesHeader,nodes));
 	};
 	
-	createManageNodePage = function(nodename){
-		var header = $("<div>").append($("<h3>").html(nodename));
-		var subheader = $("<div>").append($("<h4>").html("Manage your node here"));
-		var manage_node_page = $("<div>").attr("id", "manage_node").addClass("row-fluid tab-pane").append(header, subheader, $("<hr>"));
-		
-		$("#desktop").append(manage_node_page);
-	}
-	
-	initNodeAside = function(nodeName){
-		var nodesHeader = "<h4><i class='fa fa-sitemap fa-lg'></i>Nodes</h4>";
-		var node = $("<div>").append($("<ul>").addClass("fa-ul navigationLink").append("<li><a href='#manage_node'><i class='fa fa-minus fa-li'></i>"+nodeName+"</a></li>"));
-		$("#homeAside").append($("<div>").append(nodesHeader,node));
-	};
-	
 	createAllNodesPage = function(){
-		var header = $("<div>").append($("<h3>").html("Manage Unifi Nodes"));
-		var subheader = $("<div>").append($("<h4>").html("View the status of all connected nodes."));
+		var header = $("<h3>").html("Manage Unifi Nodes");
+		var subheader = $("<h4>").html("View the status of all connected nodes.");
 		
 		var tableHeader = $("<tr>").append($("<th>").html("Name"), $("<th>").html("Status"), $("<th>"));
 		var table = $("<table>").attr("id", "allnodes").append(tableHeader)
-		var tableDiv = $("<div>").append(table);
 		
-		var manage_node_page = $("<div>").attr("id", "nodes").addClass("row-fluid tab-pane").append(header, subheader, $("<hr>"), tableDiv);
+		var manage_node_page = $("<div>").attr("id", "nodes").addClass("row-fluid tab-pane").append(header, subheader, $("<hr>"), table);
 		
 		$("#desktop").append(manage_node_page);
+		
+		initAllNodes(false);
 	}
 	
 	initAllNodes = function(updateToo){
@@ -75,8 +74,8 @@ function(Utils, Server){
 	};
 	
 	createAddNodePage = function(){
-		var header = $("<div>").append($("<h3>").html("Add node"));
-		var subheader = $("<div>").append($("<h4>").html("Set up connection to a new node"));
+		var header = $("<h3>").html("Add node");
+		var subheader = $("<h4>").html("Set up connection to a new node");
 		
 		var label = $("<label>").addClass("span2").attr("for", "nodeName").html("Name");
 		var input = $("<input>").addClass("span8").attr("id", "nodeName").attr("type" ,"text").attr("placeholder", "The name of the node");
@@ -92,9 +91,6 @@ function(Utils, Server){
 			openDesktopTab("#nodes");
 		});
 		var buttonDiv = $("<div>").addClass("row-fluid").append(button);
-
-		
-		
 		
 		var add_node_page = $("<div>").attr("id", "addnode").addClass("row-fluid tab-pane").append(header, subheader, $("<hr>"), inputDiv, buttonDiv);
 		
